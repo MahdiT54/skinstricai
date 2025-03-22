8ddf3ee66785cd08a958ab6d74982cc7a6afe176
Mahdi T <78121487+MahdiT54@users.noreply.github.com>
2025-03-22 08:49:25 -0400

    .

--- skinstricai/src/app/page.tsx
+++ skinstricai/src/app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <div className='relative h-full flex flex-1 flex-col'>
       <div className='contents'> {/* page wrapper */}
-        <h1 className='m-auto text-center'> {/* title coontainer */}
+        <h1 className='m-auto text-center text-7xl tracking-[-0.07em] leading-[0.945] max-w-[14ch] flex flex-col'> {/* title coontainer */}
           <span>Sophisticated</span>
           <span>skincare</span>
         </h1>
-        <p>Skinstric developed an A.I. that creates a highly-personalised routine tailored to what your skin needs</p> {/* subtitle container */}
+        <p className="text-neutral-900 text-sm font-normal tracking-normal leading-relaxed max-w-xs uppercase">Skinstric developed an A.I. that creates a highly-personalised routine tailored to what your skin needs</p> {/* subtitle container */}
       </div>
       <div className='absolute top-1/2 left-0'>Discover A.I.</div> {/* left-surv */}
       <div className='absolute top-1/2 right-0'>Take Test</div> {/* right-surv */}
