6edb7b3a60cf418de4a5c1bdace0f52202fa4394
Mahdi T <78121487+MahdiT54@users.noreply.github.com>
2025-03-24 01:42:05 -0400

    forms-1 validating

--- skinstricai/src/app/components/Navbar.tsx
+++ skinstricai/src/app/components/Navbar.tsx
@@ -1,12 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
     return (
-
-        <nav className='h-16 flex justify-center fixed w-full px-8'>
+        <nav className='h-16 flex justify-center fixed w-full px-8 z-50'>
             <div className='flex justify-between items-center max-w-[1600px] w-full'>
                 <div className="text-[10px] top-4 left-4 flex items-center space-x-2 font-medium">
-                    <span className="font-bold">SKINSTRIC</span>
+                    {/* <span className="font-bold">SKINSTRIC</span> */}
+                    <Link href='/' className='font-bold'>SKINSTRIC</Link>
                     <span className="ml-6 text-gray-500">[ INTRO ]</span>
                 </div>
                 <button className="text-[10px] w-23 h-8 bg-black text-white text-xs">
--- skinstricai/src/app/testing/page.tsx
+++ skinstricai/src/app/testing/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 // import { useState } from 'react';
 
@@ -8,15 +9,19 @@ export default function Page() {
         <div className='relative h-full flex flex-col flex-1 pt-16'>
             <div className="font-semibold text-base leading-6 tracking-tight uppercase">To start analysis</div>
             <Image className='m-auto' src="/rombuses.svg" alt="rombus" width={500} height={500} />
-            <div className="absolute top-1/2 left-1/4">
-                <label htmlFor="name">Click to type</label>
+            <div className="max-w-[360px] flex flex-col items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/4">
+                <label htmlFor="name" className='font-normal text-sm leading-6 uppercase text-gray-500'>Click to type</label>
                 <input
                     type="text"
                     id="name"
                     placeholder="Introduce Yourself"
-                    className="text-4xl font-light text-center border-b-2 border-black outline-none focus:border-blue-500 transition-all"
+                    className="w-full border-b border-black text-5xl leading-tight tracking-tighter text-center"
                 />
             </div>
+            <Link href={'/'} className='flex items-center gap-4 font-semibold text-sm leading-4 tracking-tight uppercase'>
+                <Image src="/buttin-icon-shrunk.svg" alt="back btn" width={44} height={44}/>
+                Back
+            </Link>
         </div>
     );
 }
