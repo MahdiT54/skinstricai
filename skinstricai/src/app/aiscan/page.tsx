4e2f824edd9dd9814492691218731c13fb4398ae
Mahdi T <78121487+MahdiT54@users.noreply.github.com>
2025-04-01 02:07:37 -0400

    phase 2

--- skinstricai/src/app/aiscan/page.tsx
+++ skinstricai/src/app/aiscan/page.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image';
 import React from 'react';
 
 export default function Page() {
     return (
-        <></>
+        <div className='relative h-full flex flex-col flex-1 pt-16'>
+            <div className="font-semibold text-base leading-6 tracking-tight uppercase">
+                To start analysis
+            </div>
+            <div className='gap-3 flex flex-col items-center absolute top-2/3 left-1/2 -translate-x-1/2'>
+                <Image src={"/picform-middleicon.svg"} alt="middle icon" width={100} height={100}/>
+                <span className='text-neutral-900 uppercase font-normal text-[12px] leading-[16px] tracking-[0%] text-center'>select preferred way</span>
+            </div>
+        </div>
     )
 }
