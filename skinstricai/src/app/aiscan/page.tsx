f17dd5c7bafdcedffdbd3172cc2e7d62447ec23b
Mahdi T <78121487+MahdiT54@users.noreply.github.com>
2025-04-03 02:14:35 -0400

    .

--- skinstricai/src/app/aiscan/page.tsx
+++ skinstricai/src/app/aiscan/page.tsx
@@ -1,7 +1,17 @@
+'use client'
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Page() {
+    const [step, setStep] = useState(2);
+
+    const handleBack = () => {
+        if (step > 1) {
+            setStep(step - 1);
+        }
+    }
+
+
     return (
         <div className='relative h-full flex flex-col flex-1 pt-16'>
             <div className="font-semibold text-base leading-6 tracking-tight uppercase">
@@ -15,6 +25,13 @@ export default function Page() {
                 <Image src={"/picform-middleicon.svg"} alt="middle icon" width={100} height={100} />
                 <span className='text-neutral-900 uppercase font-normal text-[12px] leading-[16px] tracking-[0%] text-center'>select preferred way</span>
             </div>
+            <button
+                onClick={handleBack}
+                className='flex items-center gap-4 font-semibold text-sm leading-4 tracking-tight uppercase ml-4 mt-4'
+            >
+                <Image src="/buttin-icon-shrunk.svg" alt="back btn" width={44} height={44} />
+                Back
+            </button>
         </div>
     )
 }
